4e9881e863fc21af40a7297a3d84d77fd80c86bd
Mohamed Rahman Shareff S <62998470+rahmansk@users.noreply.github.com>
2022-06-25 00:30:08 +0530

    Create variables.py

---
+++ programs/variables.py
@@ -0,0 +1,31 @@
+# Variables are basically created at the moment when we declare it with it's value
+
+x = 2     # type = int
+y = "Python"    # type = str
+print(x)
+print(y)
+
+
+# To get the type of an variable
+
+print(type(x))    # <class 'int'>
+print(type(y))    # <class 'str'>
+
+
+# Assign using single Quotes '
+
+y = "Python"
+
+# Assign using double Quotes "
+
+y = 'Python'
+
+
+
+# Case Sensitive
+
+a = "It is a value of a"
+A = "It is a value of A"
+
+print(a)    # It is a value of a
+print(A)    # It is a value of A
